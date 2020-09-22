c299e2a407f9eb73c9d26d51f95e0b311bd0556f
Sanjoy Kumar Saha <sanjoyksaha0@gmail.com>
2020-09-23 01:48:17 +0600

    Updates

---
+++ precache-manifest.da3af2e7f500a16a43794afdbc3153c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad66423acaee11d6a33302dca5111e7",
+    "url": "/sanjoyksaha.github.io/index.html"
+  },
+  {
+    "revision": "a3a08172ec23d803b3c4",
+    "url": "/sanjoyksaha.github.io/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "062dc6f5d7fa1e45c7d8",
+    "url": "/sanjoyksaha.github.io/static/css/main.e4361cd9.chunk.css"
+  },
+  {
+    "revision": "a3a08172ec23d803b3c4",
+    "url": "/sanjoyksaha.github.io/static/js/2.dec2c01e.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/sanjoyksaha.github.io/static/js/2.dec2c01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062dc6f5d7fa1e45c7d8",
+    "url": "/sanjoyksaha.github.io/static/js/main.e6503578.chunk.js"
+  },
+  {
+    "revision": "76707f8f662d34e67906",
+    "url": "/sanjoyksaha.github.io/static/js/runtime-main.b9226f48.js"
+  },
+  {
+    "revision": "34baf4ac82a54463923fe2309a5d7ed2",
+    "url": "/sanjoyksaha.github.io/static/media/sanjoy2.34baf4ac.jpg"
+  }
+]);
